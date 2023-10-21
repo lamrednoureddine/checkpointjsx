3ce10d8440bbe3ccc203713ac6c3d729e42087fe
noureddine <noureddine.lamred@gmail.com>
2023-10-21 10:04:16 +0200

    adding nouvau correction chekpoint jsx

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import './App.css'; // Importez votre fichier CSS pour les styles.
 
 function App() {
   const { name, price, description, image } = productData;
-
+   const firstName ="zsqd"
   return (
-    <div className="app-container">
-      <h1 className="product-title">This is My Product</h1>
-      <div className="product-card">
+
+    <>
+      <h1 className="product-title"   >This is My Product</h1>
+    <div className="app-container" >
+      <div className="product-card"   >
         <img src={image} alt={name} className="product-image" />
         <h2 className="product-name">{name}</h2>
         <p className="product-price">Prix : {price}</p>
         <p className="product-description">{description}</p>
+        
+
       </div>
     </div>
+    {firstName  &&<> <img src="../smartphone.jpeg" alt='bjbhvhj'/> <h1>my phone</h1></>} 
+
+</>
   );
 }
 
